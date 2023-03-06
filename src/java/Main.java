f650c11e563be3797a5d149cb749adf0def7c736
Aleksandr K <kuchumof@gmail.com>
2023-03-06 11:21:01 +0500

    start project

---
+++ src/java/Main.java
@@ -0,0 +1,10 @@
+import java.lang.reflect.Array;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Service.check(TableCreator.getTable()));
+
+    }
+
+}
